567b2646c36cf7e4d9301579cbf13cf9477e395a
Bomy Rhee <50459262+20201141@users.noreply.github.com>
2024-11-18 15:22:04 -0500

    Update UserSettings.tsx

--- src/account/UserSettings.tsx
+++ src/account/UserSettings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 //import type { UserAccount } from '../classes/UserAccount'; 
 //import type { Post } from '../classes/Post';
 import Button from "./Button";
