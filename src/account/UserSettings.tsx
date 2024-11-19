aa325216c2035c31d1a939d183b964a83db0938e
Bomy Rhee <50459262+20201141@users.noreply.github.com>
2024-11-19 10:33:30 -0500

    Add highlight for active link in menu

--- src/account/UserSettings.tsx
+++ src/account/UserSettings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 //import type { UserAccount } from '../classes/UserAccount'; 
 //import type { Post } from '../classes/Post';
 import './UserSettings.css';
@@ -9,9 +9,9 @@ const UserSettings = () => {
     <div className="user-nav">
       <nav>
         <ul>
-          <li><Link to="/account" className="user-nav-item">User Settings</Link></li>
-          <li><Link to="/account/my-post" className="user-nav-item">My Posts</Link></li>
-          <li><Link to="/account/personalize" className="user-nav-item">Personalize</Link></li>
+          <li><NavLink to="/account" className="user-nav-item">User Settings</Link></li>
+          <li><NavLink to="/account/my-post" className="user-nav-item">My Posts</Link></li>
+          <li><NavLink to="/account/personalize" className="user-nav-item">Personalize</Link></li>
         </ul>
       </nav>
       <Outlet />
