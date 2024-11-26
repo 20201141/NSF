66bdcdab3456dcf19c3c4143ced8bb4f7e06eadf
Bomy Rhee <50459262+20201141@users.noreply.github.com>
2024-11-26 13:38:20 -0500

    add method

--- src/postDetails/PostDetails.tsx
+++ src/postDetails/PostDetails.tsx
@@ -60,7 +60,7 @@ const PostDetails: React.FC<PostDetailsProps> = ({ posts, loading }) => {
         )}
         {post.getnotif && <p>Notifications: Enabled</p>}
       </div>
-      <form action='/api/comments' className='post-details-comment-form'>
+      <form action='/api/comments' method='POST' className='post-details-comment-form'>
         <textarea name='post-content'></textarea>
         <div className='post-content-btn'>
           <button type='submit' className='comment-submit-btn'>Comment</button>
