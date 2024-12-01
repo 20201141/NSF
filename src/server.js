09d233e1eb0fc98158cc581740eb395c512dcc2e
Bomy Rhee <50459262+20201141@users.noreply.github.com>
2024-12-01 17:02:41 -0500

    change cookie config

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use(session ({
     maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
     secure: false,  // not https
     httpOnly: true, // client-side can't access cookie
-    sameSite: 'none'
+    sameSite: 'strict'
   }
 }));
 
